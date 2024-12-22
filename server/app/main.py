fcf3fe46ecee16ce3450dd610cc6f2f76fb7a475
Aayush <aayushrawatmain@gmail.com>
2024-12-22 17:38:18 +0530

    cors updated

--- server/app/main.py
+++ server/app/main.py
@@ -8,7 +8,10 @@ app = FastAPI(title="AI Insights Engine")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://employee-performance-dashboard.onrender.com",
+        "http://localhost:3000"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
